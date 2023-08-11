e6b83e39a8a8cbe903245f6d749ed735c31cacd2
Luiz Antônio Borges Martins <labm1997@gmail.com>
2023-08-11 12:27:07 -0300

    Moved test generated images to images folder

--- src/test.sh
+++ src/test.sh
@@ -1,22 +1,24 @@
+mkdir -p images
+
 echo "== Rectangular profile test =="
 echo "Encoding:"
 echo "abcdefghijklmnopqrstuvwxyz ,:.?-"
-python3 rectangular_encode.py "abcdefghijklmnopqrstuvwxyz ,:.?-" test_rectangular.png
+python3 rectangular_encode.py "abcdefghijklmnopqrstuvwxyz ,:.?-" images/test_rectangular.png
 echo "Decoding"
-python3 rectangular_decode.py test_rectangular.png 6
+python3 rectangular_decode.py images/test_rectangular.png 6
 
 echo "== Circular profile test ==
 "echo "Encoding:"
 echo "abcdefghijklmnopqrstuvwxyz ,:.?-"
-python3 circular_encode.py "abcdefghijklmnopqrstuvwxyz ,:.?-" test_circular.png --scale 4.5
+python3 circular_encode.py "abcdefghijklmnopqrstuvwxyz ,:.?-" images/test_circular.png --scale 4.5
 
 echo "Decoding"
-python3 circular_decode.py test_circular.png
+python3 circular_decode.py images/test_circular.png
 
 echo "== Circular profile test ==
 "echo "Encoding:"
 echo "voce, que tem ideias tao modernas. e o mesmo homem que vivia nas cavernas."
-python3 circular_encode.py "voce, que tem ideias tao modernas. e o mesmo homem que vivia nas cavernas." test_profile.png --scale 3.4
+python3 circular_encode.py "voce, que tem ideias tao modernas. e o mesmo homem que vivia nas cavernas." images/test_profile.png --scale 3.4
 
 echo "Decoding"
-python3 circular_decode.py test_profile.png
+python3 circular_decode.py images/test_profile.png
